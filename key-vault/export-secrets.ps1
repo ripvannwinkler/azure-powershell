838ce76dacbe36884cf82d85dafc72e05a6a7466
Chris Vann <chrisvann01@gmail.com>
2023-11-29 22:45:49 -0600

    add export-secrets

---
+++ key-vault/export-secrets.ps1
@@ -0,0 +1,17 @@
+Param(
+	[Parameter(Mandatory)]
+	# the vault to export secrets from
+	[string]$fromVault, 
+
+	[Parameter(Mandatory = $false)]
+	# the vault to import secrets to
+	[string]$toVault = "destination-vault-name"
+)
+
+$secretNames = $(az keyvault secret list --vault-name $fromVault) | ConvertFrom-Json | Select-Object -Property name
+
+$secretNames | ForEach-Object {
+	$secret = $(az keyvault secret show --name $_.name --vault-name $fromVault -o json) | ConvertFrom-Json
+	Write-Output "az keyvault secret set --vault-name $toVault --name `"$($_.name)`" --value `"$($secret.value)`" --output none"	
+}
+
