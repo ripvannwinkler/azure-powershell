451c2ce82b98246d25a857ecda71ab245ef65209
Chris Vann <chrisvann01@gmail.com>
2023-11-30 00:04:03 -0600

    output secrets as table

--- key-vault/export-secrets.ps1
+++ key-vault/export-secrets.ps1
@@ -5,17 +5,26 @@ Param(
 
 	[Parameter(Mandatory = $false)]
 	# the vault to import secrets to
-	[string]$toVault = ""
+	[string]$toVault = "destination-vault-name"
 )
 
 $secretNames = $(az keyvault secret list --vault-name $fromVault) | ConvertFrom-Json | Select-Object -Property name
 
+$secrets = @()
 $secretNames | ForEach-Object {
 	$secret = $(az keyvault secret show --name $_.name --vault-name $fromVault -o json) | ConvertFrom-Json
-	if ($toVault -eq "") {
-		Write-Output "$($_.name)=$($secret.value)"
+	$secrets += [PSCustomObject]@{
+		Name  = $_.name
+		Value = $secret.value
 	}
-	else {
-		Write-Output "az keyvault secret set --vault-name $toVault --name $($_.name) --value `"$($secret.value)`""
-	}
+}
+
+
+$secrets | Format-Table
+
+Write-Output "To import the secrets to another vault, run the following commands:"
+Write-Output ""
+
+$secrets | ForEach-Object {
+	Write-Output "az keyvault secret set --vault-name $toVault --name $($_.Name) --value `"$($_.Value)`""
 }
