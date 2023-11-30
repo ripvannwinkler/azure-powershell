f37705741f6d20c2a3250ce8d940e13ad444a457
Chris Vann <chrisvann01@gmail.com>
2023-11-29 23:41:04 -0600

    write name=value if no dest valut

--- key-vault/export-secrets.ps1
+++ key-vault/export-secrets.ps1
@@ -5,13 +5,17 @@ Param(
 
 	[Parameter(Mandatory = $false)]
 	# the vault to import secrets to
-	[string]$toVault = "destination-vault-name"
+	[string]$toVault = ""
 )
 
 $secretNames = $(az keyvault secret list --vault-name $fromVault) | ConvertFrom-Json | Select-Object -Property name
 
 $secretNames | ForEach-Object {
 	$secret = $(az keyvault secret show --name $_.name --vault-name $fromVault -o json) | ConvertFrom-Json
-	Write-Output "az keyvault secret set --vault-name $toVault --name $($_.name) --value `"$($secret.value)`" --output none"
+	if ($toVault -eq "") {
+		Write-Output "$($_.name)=$($secret.value)"
+	}
+	else {
+		Write-Output "az keyvault secret set --vault-name $toVault --name $($_.name) --value `"$($secret.value)`""
+	}
 }
-
