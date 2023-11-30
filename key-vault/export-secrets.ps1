504e3f6a4b9b77cc7115e1484bc9237324a2191b
Chris Vann <chrisvann01@gmail.com>
2023-11-30 00:05:09 -0600

    wrap commands in function

--- key-vault/export-secrets.ps1
+++ key-vault/export-secrets.ps1
@@ -8,23 +8,27 @@ Param(
 	[string]$toVault = "destination-vault-name"
 )
 
-$secretNames = $(az keyvault secret list --vault-name $fromVault) | ConvertFrom-Json | Select-Object -Property name
+function GetSecrets {
+	$secretNames = $(az keyvault secret list --vault-name $fromVault) | ConvertFrom-Json | Select-Object -Property name
 
-$secrets = @()
-$secretNames | ForEach-Object {
-	$secret = $(az keyvault secret show --name $_.name --vault-name $fromVault -o json) | ConvertFrom-Json
-	$secrets += [PSCustomObject]@{
-		Name  = $_.name
-		Value = $secret.value
+	$secrets = @()
+	$secretNames | ForEach-Object {
+		$secret = $(az keyvault secret show --name $_.name --vault-name $fromVault -o json) | ConvertFrom-Json
+		$secrets += [PSCustomObject]@{
+			Name  = $_.name
+			Value = $secret.value
+		}
+	}
+
+
+	$secrets | Format-Table
+
+	Write-Output "To import the secrets to another vault, run the following commands:"
+	Write-Output ""
+
+	$secrets | ForEach-Object {
+		Write-Output "az keyvault secret set --vault-name $toVault --name $($_.Name) --value `"$($_.Value)`""
 	}
 }
 
-
-$secrets | Format-Table
-
-Write-Output "To import the secrets to another vault, run the following commands:"
-Write-Output ""
-
-$secrets | ForEach-Object {
-	Write-Output "az keyvault secret set --vault-name $toVault --name $($_.Name) --value `"$($_.Value)`""
-}
+GetSecrets
